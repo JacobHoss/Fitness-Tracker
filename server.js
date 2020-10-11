cb3322fc3b76046add09e322155897f93495421f
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-10-11 08:16:30 -0400

    deploying

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 const app = express();
 
@@ -13,8 +24,23 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// HTML Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
+
+
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Visit the following URL to see the app locally http://localhost:${PORT}`);
 });
